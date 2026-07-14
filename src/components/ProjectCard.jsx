cb66cf109c3cf766d6f7a17701020e8f7d836868
IamLaTorya <LaTorya.Sadler@yahoo.com>
2026-07-14 16:52:55 -0500

    Updated ProjectCard to add details to Projects page

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,21 @@
-export default function ProjectCard() {
+export default function ProjectCard({ repository }) {
     return (
         <article className="project-card">
-            <h3>Project Title</h3>
-            <p>Project description goes here.</p>
+            <h3>{repository.name}</h3>
+            <p>{repository.description || "No description avaliable."}</p>
+            <p>Language: {repository.language || "Not specified"}</p>
+            {repository.homepage && (
+                <a href={repository.homepage} 
+                target="_blank"
+                rel="noopener noreferrer">
+                    Visit Website
+                </a>
+            )}
+            <a href={repository.html_url} 
+            target="_blank" 
+            rel="noopener noreferrer">
+                View on GitHub
+            </a>
         </article>
     )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,5 @@
 import useFetch from "../hooks/useFetch";//custom hook to fetch data
-// import ProjectCard from "../components/ProjectCard";
+import ProjectCard from "../components/ProjectCard";
 
 export default function Projects() {
     const { data, isLoading, error } = useFetch("https://api.github.com/users/IamLaTorya/repos");
@@ -11,20 +11,21 @@ export default function Projects() {
         return <p>{error}</p>
     }
     const featuredProjects = [
-        "jokes-app2025", 
-        "christmas-app2025", 
-        "typing-speed-app2025", 
-        "the-dynamic-task-manager", 
-        "the-digital-timekeeper", "mooncycle-ritual-garden-co-v2", "mooncycle-ritual-garden-co", 
+        "jokes-app2025",
+        "christmas-app2025",
+        "typing-speed-app2025",
+        "the-dynamic-task-manager",
+        "the-digital-timekeeper", "mooncycle-ritual-garden-co-v2", "mooncycle-ritual-garden-co",
         "rps-mirror-of-fate-game"
-    ]; 
+    ];
     const filteredProjects = data.filter((repositories) => featuredProjects.includes(repositories.name));
     return (
         <>
             <h1>Projects</h1>
-            {/* <ProjectCard /> */}
-            {filteredProjects.map((repositories) => (
-                <p key={repositories.id}>{repositories.name}</p>
+            {filteredProjects.map((repository) => (
+                <ProjectCard 
+                key={repository.id} 
+                repository={repository} />
             ))}
         </>
     )
