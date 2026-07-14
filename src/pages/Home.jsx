bd490168f891c3d63336eafd9b6fb51f3e632155
IamLaTorya <LaTorya.Sadler@yahoo.com>
2026-07-14 03:31:27 -0500

    Updated home page to include metrics and core technical stack

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ export default function Home() {
     return (
         <>
             <main className="home-main">
+                {/* Hero Section */}
                 <section className="hero">
                     <h1>LaTorya Hoyle-Sadler</h1>
                     <h2>Full Stack Software Engineer | Interactive Experience Developer</h2>
@@ -11,6 +12,7 @@ export default function Home() {
                         I build web applications, games, and educational experiences using JavaScript, React, and full-stack technologies.
                     </p>
                 </section>
+                {/* About Me Section */}
                 <section className="about-me">
                     <h2>About Me</h2>
                     <div className="about-image">
@@ -21,6 +23,32 @@ export default function Home() {
                         Through ToyMind Interactive, I explore how technology can become more than a tool by creating applications and games that inspire people to pause, interact, and discover something meaningful.
                     </p>
                 </section>
+                {/* Core Stack Section */}
+                <section className="stack">
+                    <h2>Core Stack</h2>
+                    <ul className="stack=list">
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>Node.js</li>
+                        <li>Express</li>
+                        <li>MySQL</li>
+                        <li>Git</li>
+                        <li>Bootstrap</li>
+                        <li>Tailwind CSS</li>
+                    </ul>
+                </section>
+                {/* Metrics Section */}
+                <section className="metrics">
+                    <h2>Development Highlights</h2>
+                    <div>
+                        <p> <strong>10+</strong>
+                            <br />
+                            Web Applications & Interactive Experiences</p>
+                    </div>
+                </section>
+                {/* Featured Project Section */}
                 <section className="featured-project">
                     <h2>Featured Project</h2>
                     <h3>Mooncycle Ritual Garden Co. V2</h3>
