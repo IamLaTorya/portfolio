7b65b0eaf9d5924c425176775a6132a883e92c1e
IamLaTorya <LaTorya.Sadler@yahoo.com>
2026-07-14 12:20:50 -0500

    Created ProjectCard component and updated Project page

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,8 @@
+export default function ProjectCard() {
+    return (
+        <article className="project-card">
+            <h3>Project Title</h3>
+            <p>Project description goes here.</p>
+        </article>
+    )
+}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,14 +1,10 @@
+import ProjectCard from "../components/ProjectCard";
+
 export default function Projects() {
     return (
         <>
             <h1>Projects</h1>
-            <h2>Featured Project</h2>
-            {/* mooncycle ritual garden co v2*/}
-            <h2>Games & Interactive Experiences</h2>
-            {/* Games */}
-            {/* mirror of fate */}
-            <h2>Full-Stack Application</h2>
-            {/* christmas movies database */}
+            <ProjectCard />
         </>
     )
 }
